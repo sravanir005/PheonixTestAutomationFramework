5c9a32437923b59ddcbd8cc82154951235d80f5b
sravani <sravanir005@gmail.com>
2026-09-14 19:44:58 +0530

    Created Config Manager class for Managing multiple environments (dev,qa,uat) for the project.
    Updated pom.xml - Updated maven compiler source and target to 14.

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -2,19 +2,24 @@ package com.api.tests;
 import static io.restassured.RestAssured.*;
 
 import static org.hamcrest.Matchers.*;
+
+import java.io.IOException;
+
 import org.testng.annotations.Test;
+
+import com.api.utils.ConfigManager;
 
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class LoginAPITest {
     @Test
-	public void loginAPITest() {
+	public void loginAPITest() throws IOException {
 		
 		UserCredentials userCredentials = new UserCredentials("iamfd","password");
 		
 		given()
-		    .baseUri("http://64.227.160.186:9000/v1")
+		    .baseUri(ConfigManager.getProperty("BASE_URI"))
 		.and()
 		.contentType(ContentType.JSON)
 		.and()
